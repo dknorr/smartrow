a7b2322a439d7735cb1004b8266d29b3eedb5d72
dknorr <dknorr01@gmail.com>
2020-04-19 18:40:06 -0400

    First edition of greedy complete

--- greedy.py
+++ greedy.py
@@ -3,6 +3,8 @@
 import scipy.stats as stats
 import numpy as np
 import csv
+import copy
+import random
 
 
 class Boat:
@@ -17,36 +19,52 @@ class Boat:
             self.port.append('open')
             self.starboard.append('open')
 
+    def full(self):
+        if 'open' not in (self.port + self.starboard):
+            return True
+        else:
+            return False
+
     def add_rower(self, rower, forced_placement=False):
         if rower.side == 'starboard':
-            self.starboard.append(rower)
-            self.starboard = self.starboard[1:]
+            if 'open' in self.starboard:
+                self.starboard.append(rower)
+                self.starboard.remove('open')
         else:
-            self.port.append(rower)
-            self.port = self.port[1:]
-        if not forced_placement:
-            self.total_adjusted_watts = score_boat(rower, self, True)
+            if 'open' in self.port:
+                self.port.append(rower)
+                self.port.remove('open')
+
+        self.total_adjusted_watts = score_boat(rower, self, True)
+
+    def remove_rower(self, rower):
+        if rower in self.starboard:
+            self.starboard.remove(rower)
+            self.starboard.append('open')
+        if rower in self.port:
+            self.port.remove(rower)
+            self.port.append('open')
 
     def print_lineup(self):
         lineup = []
         if self.rig == 'starboard':
             for i in range(len(self.starboard)):
                 if self.starboard[i] is not 'open':
-                    lineup.append(self.starboard[i].first)
+                    lineup.append(self.starboard[i].first + " " + self.starboard[i].last)
                 else:
                     lineup.append('open')
                 if self.port[i] is not 'open':
-                    lineup.append(self.port[i].first)
+                    lineup.append(self.port[i].first + " " + self.port[i].last)
                 else:
                     lineup.append('open')
         else:
             for i in range(len(self.port)):
                 if self.port[i] is not 'open':
-                    lineup.append(self.port[i].first)
+                    lineup.append(self.port[i].first + " " + self.port[i].last)
                 else:
                     lineup.append('open')
                 if self.starboard[i] is not 'open':
-                    lineup.append(self.starboard[i].first)
+                    lineup.append(self.starboard[i].first + " " + self.starboard[i].last)
                 else:
                     lineup.append('open')
         print(lineup)
@@ -68,6 +86,12 @@ class Rower:
         self.finish = finish
         self.peak_force_location = peak_force_location
 
+    def is_same(self, rower):
+        if self.first == rower.first and self.last == rower.last:
+            return True
+        else:
+            return False
+
 
 def weight_adjust(watts, weight):
     pace = (2.80/watts)**(1/3)
@@ -81,10 +105,10 @@ def score_boat(rower, boat, rescore=False):
     if not rescore:
         if rower.side == 'port':
             if 'open' not in boat.port:
-                return boat.total_adjusted_watts
+                return float("-inf")
         if rower.side == 'starboard':
             if 'open' not in boat.starboard:
-                return boat.total_adjusted_watts
+                return float("-inf")
 
     # what happens if we add this guy
 
@@ -184,11 +208,57 @@ def find_very_avg_guys(num, pool):
     return most_average
 
 
+def boat_difference(fleet):
+    total_power = 0
+    for boat in fleet:
+        total_power += boat.total_adjusted_watts
+    avg_power = total_power / len(fleet)
+    total_diff_from_avg = 0
+    for boat in fleet:
+        total_diff_from_avg += abs(boat.total_adjusted_watts - avg_power)
+
+    return total_diff_from_avg
+
+
+def get_key(item):
+    return item[0]
+
+
+def fleet_full(fleet, count=False):
+    open_count = 0
+    for boat in fleet:
+        athletes = boat.starboard + boat.port
+        for athlete in athletes:
+            if athlete == 'open':
+                open_count += 1
+    if count:
+        return open_count
+    else:
+        if open_count > 0:
+            return False
+        else:
+            return True
+
+
+def refresh_rower_pool(fleet):
+    original_pool = load_data()
+    used = []
+    for boat in fleet:
+        for rower in boat.starboard + boat.port:
+            if rower is not 'open':
+                used.append(rower)
+    for rower in used:
+        for another_rower in original_pool:
+            if rower.is_same(another_rower):
+                original_pool.remove(another_rower)
+    return original_pool
+
+
 def main():
-    even_one = Boat(8, 'starboard')
-    even_two = Boat(8, 'starboard')
-    even_three = Boat(8, 'starboard')
-    fleet = [even_one, even_two, even_three]
+    b1 = Boat(8, 'starboard')
+    b2 = Boat(8, 'starboard')
+    b3 = Boat(8, 'starboard')
+    fleet = [b1, b2, b3]
 
     pool = load_data()
     starter_guys = find_very_avg_guys(3, pool)
@@ -197,7 +267,66 @@ def main():
         fleet[i].add_rower(starter_guys[i], True)
         pool.remove(starter_guys[i])
 
-    print(len(pool))
+    while not fleet_full(fleet):
+        for i in range(len(fleet)):
+
+            # Create an option resulting from adding each rowing to the boat
+            options = []
+            for rower in pool:
+                temp = copy.deepcopy(fleet[i])
+                temp.add_rower(rower)
+                new_fleet = copy.deepcopy(fleet)
+                new_fleet[i] = temp
+                options.append((boat_difference(new_fleet), new_fleet))
+
+            # Cut down the number of options, specifically those that make no progress
+            # Also, order options by how similar they keep the boat
+            options = sorted(options, key=get_key)
+            for option in options:
+                prev_count = fleet_full(fleet, True)
+                new_count = fleet_full(option[1], True)
+                if prev_count <= new_count:
+                    options.remove(option)
+
+            # Pick the fleet option where boats are most similar
+            prev_count = fleet_full(fleet, True)
+            if len(options) > 1:
+                selected = options[2][1]
+            else:
+                selected = options[0][1]
+            fleet = selected
+            new_count = fleet_full(fleet, True)
+
+            # If we get stuck in a loop where adding no one keeps the boats
+            # more equal than adding someone, force an addition
+            if prev_count == new_count:
+                if 'open' in fleet[i].port + fleet[i].starboard:
+                    fleet[i].add_rower(pool[0])
+
+            # Clean up to make sure there are not duplicate rowers
+            for boat in fleet:
+                for another_boat in fleet:
+                    if boat != another_boat:
+                        for rower in boat.starboard + boat.port:
+                            for another_rower in another_boat.starboard + another_boat.port:
+                                if rower != 'open' and another_rower != 'open':
+                                    if rower.is_same(another_rower):
+                                        boat.remove_rower(rower)
+                for rower in boat.starboard + boat.port:
+                    for another_rower in boat.starboard + boat.port:
+                        if rower != another_rower:
+                            if rower != 'open' and another_rower != 'open':
+                                if rower.is_same(another_rower):
+                                    boat.remove_rower(rower)
+
+            # Bring the available rower pool up to speed
+            pool = refresh_rower_pool(fleet)
+
+    # See final lineups and scores
+    fleet[0].print_lineup()
+    fleet[1].print_lineup()
+    fleet[2].print_lineup()
+    print(boat_difference(fleet))
 
 
 if __name__ == '__main__':
