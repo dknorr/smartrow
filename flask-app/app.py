88ddc3ac3a5ecc0f3efd7c39fcfc4265923a9979
dknorr <dknorr01@gmail.com>
2020-05-15 11:36:50 -0400

    Algo fixed, first table added

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,13 +7,17 @@ import copy
 import random
 import scipy.stats as stats
 import numpy as np
+import uuid
 from flask import Flask, flash, request, redirect, url_for, make_response, render_template
 from werkzeug.utils import secure_filename
+import codecs
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'csv'}
 
 loaded_data = []
+used_guys = []
+fleet = []
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -37,27 +41,6 @@ def form():
         </html>
     """
 
-@app.route('/transform', methods=["POST"])
-def transform_view():
-    f = request.files['data_file']
-    if not f:
-        return "No file"
-
-    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-    csv_input = csv.reader(stream)
-    #print("file contents: ", file_contents)
-    #print(type(file_contents))
-    print(csv_input)
-    for row in csv_input:
-        print(row)
-
-    stream.seek(0)
-    result = transform(stream.read())
-
-    response = make_response(result)
-    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
-    return response
-
 @app.route('/upload-file', methods=['GET', 'POST'])
 def upload_file():
     global loaded_data
@@ -68,29 +51,33 @@ def upload_file():
             return redirect(request.url)
         file_object = request.files['file']
         file_stream = file_object.stream
+        data = []
+        stream = codecs.iterdecode(file_object.stream, 'utf-8')
+        for row in csv.reader(stream, dialect=csv.excel):
+            if row:
+                data.append(row)
         # if user does not select file, browser also
         # submit an empty part without filename
         if file_object.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file_object and allowed_file(file_object.filename):
-            print("got a good file", file=sys.stderr)
             filename = secure_filename(file_object.filename)
-            rowers = []
+            rowers = {}
             line_count = 0
-            for row in file_stream:
+            for row in data:
                 if line_count == 0:
                     line_count += 1
                 else:
                     # erg, weight, height, catch, slip, wash, finish, peak_force_location, first, last, side
                     athlete = Rower(int(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]),
                             float(row[6]), float(row[7]), str(row[8]), str(row[9]), str(row[10]))
-                    rowers.append(athlete)
+                    new_rower_id = uuid.uuid4()
+                    rowers[new_rower_id] = athlete
                     line_count += 1
             loaded_data = rowers
             boatings = make_boatings()
-            print(len(rowers), file=sys.stderr)
-            return render_template("lineups.html", lineups = boatings)
+            return render_template("lineups.html", lineups = boatings, data = loaded_data)
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -148,12 +135,13 @@ if __name__ == '__main__':
 
 # Below here is boat filling algorithm
 class Boat:
-    def __init__(self, size, rig):
+    def __init__(self, size, rig, name):
         self.rig = rig
         self.size = size
         self.port = []
         self.starboard = []
         self.total_adjusted_watts = 0.0
+        self.name = name
 
         for i in range(size // 2):
             self.port.append('open')
@@ -166,7 +154,7 @@ class Boat:
             return False
 
     def add_rower(self, rower, forced_placement=False):
-        if rower.side == 'starboard':
+        if loaded_data[rower].side == 'starboard':
             if 'open' in self.starboard:
                 self.starboard.append(rower)
                 self.starboard.remove('open')
@@ -175,7 +163,7 @@ class Boat:
                 self.port.append(rower)
                 self.port.remove('open')
 
-        self.total_adjusted_watts = score_boat(rower, self, True)
+        self.total_adjusted_watts = round(score_boat(rower, self, True),2)
 
     def remove_rower(self, rower):
         if rower in self.starboard:
@@ -190,21 +178,21 @@ class Boat:
         if self.rig == 'starboard':
             for i in range(len(self.starboard)):
                 if self.starboard[i] is not 'open':
-                    lineup.append(self.starboard[i].first + " " + self.starboard[i].last)
+                    lineup.append(loaded_data[self.starboard[i]].first + " " + loaded_data[self.starboard[i]].last)
                 else:
                     lineup.append('open')
                 if self.port[i] is not 'open':
-                    lineup.append(self.port[i].first + " " + self.port[i].last)
+                    lineup.append(loaded_data[self.port[i]].first + " " + loaded_data[self.port[i]].last)
                 else:
                     lineup.append('open')
         else:
             for i in range(len(self.port)):
                 if self.port[i] is not 'open':
-                    lineup.append(self.port[i].first + " " + self.port[i].last)
+                    lineup.append(loaded_data[self.port[i]].first + " " + loaded_data[self.port[i]].last)
                 else:
                     lineup.append('open')
                 if self.starboard[i] is not 'open':
-                    lineup.append(self.starboard[i].first + " " + self.starboard[i].last)
+                    lineup.append(loaded_data[self.starboard[i]].first + " " + loaded_data[self.starboard[i]].last)
                 else:
                     lineup.append('open')
         print(lineup)
@@ -213,10 +201,10 @@ class Boat:
 
 def score_boat(rower, boat, rescore=False):
     if not rescore:
-        if rower.side == 'port':
+        if loaded_data[rower].side == 'port':
             if 'open' not in boat.port:
                 return float("-inf")
-        if rower.side == 'starboard':
+        if loaded_data[rower].side == 'starboard':
             if 'open' not in boat.starboard:
                 return float("-inf")
 
@@ -238,12 +226,12 @@ def score_boat(rower, boat, rescore=False):
     finishes = []
     peak_force_locations = []
     for some_rower in all_rowers:
-        heights.append(some_rower.height)
-        catches.append(some_rower.catch)
-        slips.append(some_rower.slip)
-        washes.append(some_rower.wash)
-        finishes.append(some_rower.finish)
-        peak_force_locations.append(some_rower.peak_force_location)
+        heights.append(loaded_data[some_rower].height)
+        catches.append(loaded_data[some_rower].catch)
+        slips.append(loaded_data[some_rower].slip)
+        washes.append(loaded_data[some_rower].wash)
+        finishes.append(loaded_data[some_rower].finish)
+        peak_force_locations.append(loaded_data[some_rower].peak_force_location)
 
     accumulated_penalties = []
     for i in range(len(all_rowers)):
@@ -263,58 +251,44 @@ def score_boat(rower, boat, rescore=False):
 
     # Apply calculated penalties
     for i in range(len(all_rowers)):
-        new_total_adjust_watts += all_rowers[i].weight_adjusted_erg * accumulated_penalties[i]
+        new_total_adjust_watts += loaded_data[all_rowers[i]].weight_adjusted_erg * accumulated_penalties[i]
 
     return new_total_adjust_watts
 
 
 def load_data():
     global loaded_data
-    rowers = loaded_data
-    # with open('rowdata.csv') as csv_file:
-    #     csv_reader = csv.reader(csv_file, delimiter=',')
-    #     line_count = 0
-    #     for row in csv_reader:
-    #         if line_count == 0:
-    #             line_count += 1
-    #         else:
-    #             # erg, weight, height, catch, slip, wash, finish, peak_force_location, first, last, side
-    #             athlete = Rower(int(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]),
-    #                             float(row[6]), float(row[7]), str(row[8]), str(row[9]), str(row[10]))
-    #             rowers.append(athlete)
-    #             line_count += 1
-    return rowers
+    return loaded_data
 
 
-def find_very_avg_guys(num, pool):
+def find_very_avg_guys(num):
     averages = []
     metrics = ['raw_erg', 'weight', 'height', 'catch',
                'slip', 'wash', 'finish', 'peak_force_location']
     for metric in metrics:
         values = []
-        for rower in pool:
-            values.append(getattr(rower, metric))
+        for rower in loaded_data.keys():
+            values.append(getattr(loaded_data[rower], metric))
         averages.append(np.mean(values))
     average_rower = Rower(averages[0], averages[1], averages[2], averages[3], averages[4], averages[5], averages[6],
                           averages[7], 'Average', 'Guy', 'None')
 
     rowers_with_deltas = []
-    for rower in pool:
+    for rower in loaded_data.keys():
         diffs = []
         for metric in metrics:
-            this_guy = getattr(rower, metric)
+            this_guy = getattr(loaded_data[rower], metric)
             avg_guy = getattr(average_rower, metric)
             diffs.append(abs(this_guy - avg_guy))
         total_delta = sum(diffs)
-        rower.delta = total_delta
+        loaded_data[rower].delta = total_delta
         rowers_with_deltas.append(rower)
 
     # data_type = [('delta', float), ('athlete', Rower)]
     # temp = np.array(rowers_with_deltas, dtype=data_type)
     # temp = np.sort(temp, order='delta')
-    rowers_with_deltas.sort(key=lambda x: x.delta)
+    rowers_with_deltas.sort(key=lambda x: loaded_data[x].delta)
 
-    print(len(rowers_with_deltas), file=sys.stderr)
     most_average = []
     for i in range(num):
         most_average.append(rowers_with_deltas[i])
@@ -355,95 +329,56 @@ def fleet_full(fleet, count=False):
 
 
 def refresh_rower_pool(fleet):
-    original_pool = load_data()
     used = []
     for boat in fleet:
         for rower in boat.starboard + boat.port:
             if rower is not 'open':
                 used.append(rower)
-    for rower in used:
-        for another_rower in original_pool:
-            if rower.is_same(another_rower):
-                original_pool.remove(another_rower)
-    return original_pool
-
+    global used_guys
+    used_guys = used
 
 def make_boatings():
-    b1 = Boat(8, 'starboard')
-    b2 = Boat(8, 'starboard')
-    b3 = Boat(8, 'starboard')
-    fleet = [b1, b2, b3]
+    global fleet
+    global used_guys
+    b1 = Boat(4, 'starboard', 'Jefferson')
+    b2 = Boat(4, 'port', 'Pippin')
+    b3 = Boat(4, 'starboard', 'Critchfield')
+    b4 = Boat(4, 'port', 'Cusano')
+    b5 = Boat(4, 'starboard', 'Kudravetz')
+    fleet = [b1, b2, b3, b4, b5]
 
-    pool = load_data()
-    starter_guys = find_very_avg_guys(3, pool)
+    starter_guys = find_very_avg_guys(len(fleet))
 
     for i in range(len(fleet)):
         fleet[i].add_rower(starter_guys[i], True)
-        pool.remove(starter_guys[i])
+        used_guys.append(starter_guys[i])
 
     while not fleet_full(fleet):
         for i in range(len(fleet)):
-
-            # Create an option resulting from adding each rowing to the boat
-            options = []
-            for rower in pool:
-                temp = copy.deepcopy(fleet[i])
-                temp.add_rower(rower)
-                new_fleet = copy.deepcopy(fleet)
-                new_fleet[i] = temp
-                options.append((boat_difference(new_fleet), new_fleet))
-
-            # Cut down the number of options, specifically those that make no progress
-            # Also, order options by how similar they keep the boat
-            options = sorted(options, key=get_key)
-            for option in options:
-                prev_count = fleet_full(fleet, True)
-                new_count = fleet_full(option[1], True)
-                if prev_count <= new_count:
-                    options.remove(option)
-
-            # Pick the fleet option where boats are most similar
-            print(options, file=sys.stderr)
+             # Create an option resulting from adding each rowing to the boat
+            options = [(0, fleet)]
+            for rower in loaded_data.keys():
+                if rower not in used_guys:
+                    fleet_full(fleet, True)
+                    side = loaded_data[rower].side
+                    first = loaded_data[rower].first
+                    if side == 'starboard':
+                        if 'open' in fleet[i].starboard:
+                            new_fleet = copy.deepcopy(fleet)
+                            new_fleet[i].add_rower(rower) 
+                            options.append((boat_difference(new_fleet), new_fleet))
+                    if side == 'port':
+                        if 'open' in fleet[i].port:
+                            new_fleet = copy.deepcopy(fleet)
+                            new_fleet[i].add_rower(rower) 
+                            options.append((boat_difference(new_fleet), new_fleet))
             prev_count = fleet_full(fleet, True)
-            if(len(options) >= 1):
-                 print("we have options!!!", file=sys.stderr)   
-            else:
-                 print("No options", file=sys.stderr) 
             if len(options) > 1:
                 selected = options[2][1]
             else:
                 selected = options[0][1]
-            fleet = selected
+            fleet[i] = selected[i]
             new_count = fleet_full(fleet, True)
 
-            # If we get stuck in a loop where adding no one keeps the boats
-            # more equal than adding someone, force an addition
-            if prev_count == new_count:
-                if 'open' in fleet[i].port + fleet[i].starboard:
-                    fleet[i].add_rower(pool[0])
-
-            # Clean up to make sure there are not duplicate rowers
-            for boat in fleet:
-                for another_boat in fleet:
-                    if boat != another_boat:
-                        for rower in boat.starboard + boat.port:
-                            for another_rower in another_boat.starboard + another_boat.port:
-                                if rower != 'open' and another_rower != 'open':
-                                    if rower.is_same(another_rower):
-                                        boat.remove_rower(rower)
-                for rower in boat.starboard + boat.port:
-                    for another_rower in boat.starboard + boat.port:
-                        if rower != another_rower:
-                            if rower != 'open' and another_rower != 'open':
-                                if rower.is_same(another_rower):
-                                    boat.remove_rower(rower)
-
-            # Bring the available rower pool up to speed
-            pool = refresh_rower_pool(fleet)
-
-    # See final lineups and scores
-    fleet[0].print_lineup()
-    fleet[1].print_lineup()
-    fleet[2].print_lineup()
-    print(boat_difference(fleet))
+            refresh_rower_pool(fleet)
     return fleet
